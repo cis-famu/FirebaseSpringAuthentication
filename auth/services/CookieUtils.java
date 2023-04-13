977e8566e9e28f4bb0b8aaba1e78110a1e6309d4
vacoote89 <vanessa.coote@gmail.com>
2023-04-13 00:53:53 -0400

    Update CookieUtils.java

--- auth/services/CookieUtils.java
+++ auth/services/CookieUtils.java
@@ -1,6 +1,3 @@
-package com.vac.blog.auth.services;
-
-import com.vac.blog.auth.models.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
